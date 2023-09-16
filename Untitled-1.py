fa5228a347520812927e04012a7d54160b0c7258
am <tfo@gmail.com>
2023-09-16 22:53:47 +0100

    hm

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+for i in range(1,100):
+    if i % 3 == True:
+        print(i,'BUZZ')
+    if i % 5 == True:
+        print(i,'FIZZ')
+    if i % 5 and i % 3 == True:
+        print(i,'FIZZBUZZ')
+    else:
+        print(i,'number doesnt exist')e
---
+++ iwa.py
@@ -0,0 +1,5 @@
+nbr = int(input("enter a number"))
+sum = 1
+for i in range(1,nbr):
+    sum = sum * i*i
+    print(sum)
